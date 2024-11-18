34f9a1d57f855c90a199ecc7e6b3959917d695fb
Kevin Lowe <lowekevin123@gmail.com>
2024-11-18 14:47:51 -0500

    simple web server

--- cmd/snippetbox/main.go
+++ cmd/snippetbox/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// register route
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// starting server
+	log.Print("starting server on: 4000")
+
+	// ListenAndServe to start the server
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
