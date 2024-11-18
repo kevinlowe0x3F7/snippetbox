f522cc207314535045e940be72ba10c7ea9d3135
Kevin Lowe <lowekevin123@gmail.com>
2024-11-18 15:02:32 -0500

    more routes

--- cmd/snippetbox/main.go
+++ cmd/snippetbox/main.go
@@ -9,10 +9,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
 func main() {
 	// register route
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// starting server
 	log.Print("starting server on: 4000")
